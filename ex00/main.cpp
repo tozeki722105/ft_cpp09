df22ffb31fd2100ff61b7e6c97c577e2bf0c4e6a
toshihiro ozeki <you@example.com>
2024-12-10 22:05:22 +0900

    add: ex00

---
+++ ex00/main.cpp
@@ -0,0 +1,13 @@
+int main(int argc, char **argv)
+{
+	// csv_map作成
+	// - csvのvalidate
+
+	// (繰り返し){
+	// input.txt を読み込み込む
+	// if (validate) 日付自体、それより前のcsv_dataがない、0~1000over
+	// - 出力
+	// else
+	// - エラー文
+	// }
+}
